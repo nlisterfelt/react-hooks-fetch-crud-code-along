629123e3dedb492bdc8b8076ade68ff81834ea5a
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-08 11:49:47 -0500

    CRUD

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,36 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+  function handleAddToCartClick(){
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: 'PATCH',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        isInCart: !item.isInCart
+      })
+    })
+    .then(r=>r.json())
+    .then(data=>onUpdateItem(data))
+  }
+  function handleDeleteClick(){
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: 'DELETE'
+    })
+    .then(r=>r.json())
+    .then(data=>onDeleteItem(data))
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button 
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
